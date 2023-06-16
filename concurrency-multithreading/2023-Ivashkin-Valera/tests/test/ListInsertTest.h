d611c68195a8a768df50be85b1f585f6c1d9ee92
Nesi01 <valerawow01@mail.ru>
2023-06-16 15:37:16 +0300

    Ivashkin upload

---
+++ concurrency-multithreading/2023-Ivashkin-Valera/tests/test/ListInsertTest.h
@@ -0,0 +1,69 @@
+#ifndef LISTINSERTTEST_H
+#define LISTINSERTTEST_H
+
+#include "Test.h"
+#include "../Utils/LinkedList.h"
+
+class ListInsertTest: public NumbersTest {
+public:
+    typedef Utils::LinkedList<int> MyList;
+
+    virtual void setup() {
+        m_sharedList.clear();
+    }
+
+    virtual void teardown() {
+        m_sharedList.clear();
+    }
+
+    virtual bool check() {
+        std::vector<int> inputSorted(m_input);
+        std::vector<int> resultSorted;
+        resultSorted.reserve(inputSorted.size());
+
+        for(MyList::Iterator it = m_sharedList.begin(); it != m_sharedList.end(); it++) {
+            resultSorted.push_back(it.value());
+        }
+
+        if (resultSorted.size() != inputSorted.size()) {
+            return false;
+        }
+
+        std::sort(inputSorted.begin(), inputSorted.end());
+        std::sort(resultSorted.begin(), resultSorted.end());
+
+        for (size_t i = 0; i < inputSorted.size(); i++) {
+            const int srcValue = inputSorted[i];
+            const int listValue = resultSorted[i];
+
+            if (srcValue != listValue) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+protected:
+
+    virtual void worker(size_t start, size_t end) {
+        for(size_t i = start; i < end; i++) {
+            //bool back = (rand() % 2);
+            bool back = (m_input[i] % 2);
+            
+            {
+                TX_RAII;
+                if (back) {
+                    m_sharedList.pushBack(m_input[i]);
+                } else {
+                    m_sharedList.pushFront(m_input[i]);
+                }
+            }
+        }
+    }
+
+    MyList m_sharedList;
+};
+
+
+#endif // LISTINSERTTEST_H
---
+++ concurrency-multithreading/2023-Ivashkin-Valera/tests/test/TreeInsertTest.h
@@ -0,0 +1,54 @@
+#ifndef TREEINSERTTEST_H
+#define TREEINSERTTEST_H
+
+#include "Test.h"
+
+#include "../Utils/TreeSet.h"
+
+class TreeInsertTest: public NumbersTest {
+public:
+    typedef Utils::TreeSet<int> MySet;
+
+    virtual void setup() {
+        m_sharedSet.clear();
+    }
+
+    virtual void teardown() {
+        m_sharedSet.clear();
+    }
+
+    virtual bool check() {
+        std::vector<int> inputSorted(m_input);
+
+        size_t i = 0;
+        std::sort(inputSorted.begin(), inputSorted.end());
+
+        for (MySet::Iterator it = m_sharedSet.begin(); it != m_sharedSet.end(); it++) {
+            const int srcKey = inputSorted[i];
+            const int setKey = it.key();
+
+            if (srcKey != setKey) {
+                return false;
+            }
+
+            i++;
+        }
+
+        return true;
+    }
+
+protected:
+    virtual void worker(size_t start, size_t end) {
+        for(size_t i = start; i < end; i++) {
+            {
+                TX_RAII;
+                m_sharedSet.insertMulti(m_input[i]);
+            }
+        }
+    }
+
+    MySet m_sharedSet;
+};
+
+
+#endif // TREEINSERTTEST_H
